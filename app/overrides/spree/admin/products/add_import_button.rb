5dd8c63789eb095cbacc2aacd5f2763537ce913c
Marat Khusainov <mahys116@gmail.com>
2018-09-25 14:58:23 +0200

    add import csv button and modal

---
+++ app/overrides/spree/admin/products/add_import_button.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path: 'spree/admin/products/index',
+                     name: 'add_import_csv_button',
+                     insert_before: "erb[silent]:contains('if can?(:create, Spree::Product)')",
+                     partial: 'spree/admin/products/import_csv_button')
